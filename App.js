eb3c8dfee3b965b1404093e0e60770fc872bb3a8
kevin <kevinalvapearson777@gmail.com>
2020-09-25 23:31:52 -0500

    pantalla inicial app

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Image, Button, Text, View } from 'react-native';
 
 export default function App() {
+
+  let [value, setValue] = useState(0);
+
+  const restar = () => {
+    if (value > 0)
+    setValue(--value)
+  }
+
+  const sumar = () => {
+    setValue(++value)
+  }
+
+  const resetear = () => {
+    setValue(0)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.votaciones}>Votaciones Partido Creo</Text>
+      <Image
+        style={styles.image}
+        source={{
+          uri: 'https://pbs.twimg.com/media/A7I_mNsCIAAYO5h.jpg',
+        }}
+      />
+      <View style={styles.wrapper}>
+        <button
+          onPress={restar}
+          
+        >-</button>
+        <View>{value}</View>
+        <button
+          onPress={sumar}
+          color="white"
+        >+</button>
+      </View>
     </View>
   );
 }
@@ -18,4 +50,37 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  votaciones: {
+    color: 'midnightblue', 
+    fontWeight: 'bold', 
+    fontSize: '20px'
+  },
+  button: {
+    backGroundColor: 'white'
+  },
+  app: {
+    fontFamily: 'sans serif',
+    textAlign: 'center',
+    fontSize: '25px'
+  }, 
+  image: {
+    height: '450px',
+    width: '250px',
+    marginTop: '20px',
+    marginBottom: '20px'
+  },
+  wrapper: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    height: '100px',
+    width: '80%',
+    margin: 'auto',
+    backgroundColor: 'skyblue',
+    fontSize: '60px', 
+    flexDirection: 'row'
+    
+  }
+
 });
+
